3207706dce16369b416525c0c6da6b0a580af583
phuongvo9 <huyphuong4116@gmail.com>
2021-10-31 12:25:52 +0700

    add terraform build file

---
+++ terraform-1.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+#sed -i -e 's/<PROJECT_ID>/'$DEVSHELL_PROJECT_ID/ main.tf
