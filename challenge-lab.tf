e4d67393bfb44287248fe21c23eaeed278841a16
phuongvo9 <huyphuong4116@gmail.com>
2021-11-05 16:20:31 +0700

    add challenge-lab file

---
+++ challenge-lab.tf
@@ -0,0 +1,14 @@
+# Import existing infrastructure into your Terraform configuration.
+
+# Build and reference your own Terraform modules.
+
+# Add a remote backend to your configuration.
+
+# Use and implement a module from the Terraform Registry.
+
+# Re-provision, destroy, and update infrastructure.
+
+# Test connectivity between the resources you've created
+
+
+
