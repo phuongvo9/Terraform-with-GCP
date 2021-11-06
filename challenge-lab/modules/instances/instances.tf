8bfe8c688af8e9c75f772e6feb35d28abe0227c1
phuongvo9 <huyphuong4116@gmail.com>
2021-11-06 14:51:33 +0700

    Taint and Destroy 3rd instance

--- challenge-lab/modules/instances/instances.tf
+++ challenge-lab/modules/instances/instances.tf
@@ -98,3 +98,75 @@ resource "google_compute_instance" "tf-instance-273537" {
     EOT
    allow_stopping_for_update = true
 }
+
+
+#### TASK 5 Taint 3rd instance
+
+resource "google_compute_instance" "tf-instance-1" {
+  name = "tf-instance-1"
+  
+  # Task 4 - Change machine type
+  machine_type = "n1-standard-2"
+  zone = var.zone
+  boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+
+}
+resource "google_compute_instance" "tf-instance-2" {
+  name = "tf-instance-2"
+  
+  # Change from n1-standard-1 to n1-standard-2
+  machine_type = "n1-standard-2"
+  zone = var.zone
+  boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+# Remove 3rd instance
+# resource "google_compute_instance" "tf-instance-273537" {
+#   name = "tf-instance-273537"
+  
+#   # Change from n1-standard-1 to n1-standard-2
+#   machine_type = "n1-standard-2"
+#   zone = var.zone
+#   boot_disk {
+#     initialize_params {
+#         image = "debian-cloud/debian-10"
+#     }
+#   }
+#   network_interface {
+#     network = "default"
+#   }
+#   metadata_startup_script = <<-EOT
+#         #!/bin/bash
+#     EOT
+#    allow_stopping_for_update = true
+# }
+
+Name
+tf-instance-273537
+Instance Id
+
+4710153760417748154
+
+terraform import module.google_compute_instance.tf-instance-273537 4710153760417748154
