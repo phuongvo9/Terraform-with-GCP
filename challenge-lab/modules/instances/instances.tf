1afba44dcec19b9be347082a734e120eb1b12760
phuongvo9 <huyphuong4116@gmail.com>
2021-11-06 20:31:54 +0700

    update main.tf

--- challenge-lab/modules/instances/instances.tf
+++ challenge-lab/modules/instances/instances.tf
@@ -163,10 +163,49 @@ resource "google_compute_instance" "tf-instance-2" {
 #    allow_stopping_for_update = true
 # }
 
-Name
-tf-instance-273537
-Instance Id
 
-4710153760417748154
+### TASK 6 - Attach VM to the subnet VPC
+resource "google_compute_instance" "tf-instance-1" {
+  name = "tf-instance-1"
+  machine_type = "n1-standard-2"
+  zone = var.zone
+  boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    #network = "default"
+    # Change for task 6 - attach to new subnet
+    network = "tf-vpc-179675"
+    subnetwork = "subnet-01"
 
-terraform import module.google_compute_instance.tf-instance-273537 4710153760417748154
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+
+}
+resource "google_compute_instance" "tf-instance-2" {
+  name = "tf-instance-2"
+  # Change from n1-standard-1 to n1-standard-2
+  machine_type = "n1-standard-2"
+  zone = var.zone
+  boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    #network = "default"
+    # Change for task 6 - attach to new subnet
+    network = "tf-vpc-179675"
+    subnetwork = "subnet-02"
+
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
