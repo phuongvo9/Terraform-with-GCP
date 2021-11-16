4c6a6fed5b33e132fd91ced1b1853df6e82d2d00
phuongvo9 <huyphuong4116@gmail.com>
2021-11-16 19:10:59 +0700

    add main.tf

--- HTTPS-LoadBalancer-Terraform/HTTPS-LoadBalancer-Terraform.tf
+++ HTTPS-LoadBalancer-Terraform/HTTPS-LoadBalancer-Terraform.tf
@@ -2,4 +2,6 @@
 # Configure Terraform in the Google Cloud environment
 # Create a global HTTPS Content-Based Load Balancer
 
+terraform init
+terraform plan -out=tfplan -var 'project=qwiklabs-gcp-00-012e3ba5a944'
 
