3817d87ca831c839d23fd8f7be4d6b47623b6aca
phuongvo9 <huyphuong4116@gmail.com>
2021-11-16 19:18:38 +0700

    add file

--- HTTPS-LoadBalancer-Terraform/HTTPS-LoadBalancer-Terraform.tf
+++ HTTPS-LoadBalancer-Terraform/HTTPS-LoadBalancer-Terraform.tf
@@ -5,3 +5,6 @@
 terraform init
 terraform plan -out=tfplan -var 'project=qwiklabs-gcp-00-012e3ba5a944'
 
+EXTERNAL_IP=$(terraform output | grep load-balancer-ip | cut -d = -f2 | xargs echo -n)
+
+echo https://${EXTERNAL_IP}
